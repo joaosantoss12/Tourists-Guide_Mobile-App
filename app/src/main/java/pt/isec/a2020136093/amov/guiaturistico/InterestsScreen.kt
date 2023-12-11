4fefdce11334cf82742d0092829505ed7129265c
biamaia103 <bia.maia103@gmail.com>
2023-12-11 22:19:44 +0000

    alteração n«minima da edição da categoria

--- app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/InterestsScreen.kt
+++ app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/InterestsScreen.kt
@@ -222,6 +222,7 @@ fun InterestsScreen(
                             AddCategoryButton(
                                 onClick = {
                                     navController.navigate("AddForm")
+                                    viewModel.tipoAddForm.value = "Categoria"
                                 },
                             )
                         }
