0d3a056f7a97bd208a19f8f6cbca37321b948951
biamaia103 <bia.maia103@gmail.com>
2023-12-15 00:58:55 +0000

    created comments UI

--- app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/screens/CommentsScreen.kt
+++ app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/screens/CommentsScreen.kt
@@ -1,8 +1,130 @@
 package pt.isec.a2020136093.amov.guiaturistico.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import pt.isec.a2020136093.amov.guiaturistico.R
+import pt.isec.a2020136093.amov.guiaturistico.viewModel.FirebaseViewModel
+import pt.isec.a2020136093.amov.guiaturistico.viewModel.FirebaseViewModel.Companion.categorias
 
 @Composable
-fun CommentsScreen() {
+fun CommentsScreen(
+    viewModel: FirebaseViewModel,
+    navController: NavController
+) {
 
+    viewModel.getComentarios()
+    val comentarios = FirebaseViewModel.comentarios
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp)
+    ) {
+
+        Text(
+            text = stringResource(R.string.comentarios),
+            lineHeight = 30.sp,
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily(Font(R.font.font)),
+            color = Color(42, 54, 66, 255),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 0.dp, 16.dp, 20.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(0.dp, 15.dp, 0.dp, 0.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            comentarios.value?.forEach { comentario ->
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        elevation = CardDefaults.cardElevation(10.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.White
+                        )
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+
+                            Text(
+                                text = comentario.texto,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(15.dp),
+                                maxLines = 3,
+                                fontFamily = FontFamily.Serif,
+                                fontSize = 13.sp,
+                                color = Color.Black
+                            )
+
+                            Row (
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(7.dp),
+                            ){
+                                Text(
+                                    text = comentario.email,
+                                    modifier = Modifier,
+                                    style = TextStyle(
+                                        textAlign = TextAlign.End
+                                    )
+                                )
+
+                                Text(
+                                    text = comentario.data.toDate().toString(),
+                                    modifier = Modifier,
+                                    style = TextStyle(
+                                        textAlign = TextAlign.Start
+                                    )
+                                )
+                            }
+
+
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(20.dp))
+            }
+        }
+    }
 }
