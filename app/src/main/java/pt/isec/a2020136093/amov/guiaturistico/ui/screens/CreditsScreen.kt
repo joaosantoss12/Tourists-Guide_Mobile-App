6456ef6a2be3cecd69c613dcbbf504b27d617e09
biamaia103 <bia.maia103@gmail.com>
2023-12-29 16:22:35 +0000

    creditos

--- app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/ui/screens/CreditsScreen.kt
+++ app/src/main/java/pt/isec/a2020136093/amov/guiaturistico/ui/screens/CreditsScreen.kt
@@ -17,12 +17,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -42,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -56,77 +59,93 @@ import androidx.navigation.NavController
 @Composable
 fun CreditsScreen(
     navController: NavController,
-    modifier : Modifier = Modifier
+    modifier: Modifier = Modifier
 ) {
-
-    Column(
-        verticalArrangement = Arrangement.Center,
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFF0F5891), // Cor inicial do gradiente
+                        Color(0xFF5EABE7)  // Cor final do gradiente
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "CRÉDITOS",
-            textAlign = TextAlign.Center,
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Serif,
-            color = Color.Black,
+        Card(
             modifier = Modifier
-                .fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(50.dp))
+                .widthIn(max = 400.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "CRÉDITOS",
+                    textAlign = TextAlign.Center,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = "Beatriz Isabel Inácio Maia",
-            textAlign = TextAlign.Center,
-            fontSize = 23.sp,
-            fontFamily = FontFamily.Serif,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Text(
-            text = "2020128841",
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Serif,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+                Text(
+                    text = "Beatriz Isabel Inácio Maia",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = "2020128841",
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-        Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = "João Miguel Duarte dos Santos",
-            textAlign = TextAlign.Center,
-            fontSize = 23.sp,
-            fontFamily = FontFamily.Serif,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Text(
-            text = "2020136093",
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Serif,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+                Text(
+                    text = "João Miguel Duarte dos Santos",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Text(
+                    text = "2020136093",
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily.Serif,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
 
-        Spacer(modifier = Modifier.height(50.dp))
-        Text(
-            text = "APLICAÇÕES MÓVEIS", // Replace with the subject name
-            fontSize = 30.sp,
-            fontFamily = FontFamily.Serif,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "APLICAÇÕES MÓVEIS",
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily.Serif,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    color = Color.DarkGray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
     }
 }
-
-
 @Preview
 @Composable
 fun CreditsScreenPreview() {
